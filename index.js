ad0643c19cf4482f64006b4f54912c37519218db
Your Name <shahanricky86@gmail.com>
2023-05-20 11:57:56 +0600

    get-data-using-id

--- index.js
+++ index.js
@@ -43,6 +43,12 @@ async function run() {
             res.send(result)
         })
 
+        app.get("/carToys/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await carToysCollection.findOne(query)
+            res.send(result);
+        })
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
